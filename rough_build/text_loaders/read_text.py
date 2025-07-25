ad99093a7a31d4cce99dda08e63f632039d15548
Ivan Torriani <ivantorriani@Mac.attlocal.net>
2025-07-24 22:35:03 -0700

    * set up rough build organization * fixed import errors brute force (i not focusing on that)

---
+++ rough_build/text_loaders/read_text.py
@@ -0,0 +1,11 @@
+#=========================
+'''
+file: read-text.py
+purpose: read txt files and return raw strings
+'''
+#=========================
+
+def readtxt(path:str) -> str:    
+    with open("text-data/" + path, 'r', encoding="utf-8") as f:
+        raw_text = f.read()
+        return raw_text
---
+++ rough_build/tokenizers/simple_tokenizers.py
@@ -0,0 +1,74 @@
+#=========================
+'''
+file: Simple-Tokenizers.py
+purpose: present two simple methods for decoding and encoding text
+'''
+#=========================
+
+import re
+from rough_build.text_loaders.read_text import readtxt
+
+#process text===========================================================================
+
+raw_text = readtxt("news_story.txt")
+
+preprocess_pass_one = re.split(r'([,.:;?_!"()\s]|--)', raw_text)
+preprocess_pass_two = [item.strip() for item in preprocess_pass_one if item.strip()]
+
+
+#vocabulary===========================================================================
+
+alphabetical_words = sorted(set(preprocess_pass_two))
+alphabetical_words.extend(["<|endoftext|>", "<|unk|>"])
+vocabulary = {token:interger for interger, token in enumerate(alphabetical_words)}
+
+# tokenizers (not handle unknowns, handles unknowns) ==================================================
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+    
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\s]|--)', text)
+                                
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+        
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = { i:s for s,i in vocab.items()}
+    
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [
+            item if item in self.str_to_int 
+            else "<|unk|>" for item in preprocessed
+        ]
+
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+        
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # Replace spaces before the specified punctuations
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
+        return text
+
+# example ==================================================
+
+tokenizer = SimpleTokenizerV1(vocabulary)
+tokenizer_2 = SimpleTokenizerV2(vocabulary)
+
+print(tokenizer.encode(raw_text)) 
+print(tokenizer_2.encode(raw_text))
