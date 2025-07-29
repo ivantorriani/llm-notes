9b9eec10b0507f4b12126e30fd473a91e27ffd1a
Ivan Torriani <sotivantorriani@gmail.com>
2025-07-28 18:04:55 -0700

    feat *set up casual attention class

---
+++ rough_build/SelfAttention/CasualAttention.py
@@ -0,0 +1,11 @@
+#=========================
+'''
+file: CasualAttention.py
+purpose: Implement a casual attention class (self attention + masking )
+'''
+#=========================
+
+#imports=========================
+import torch
+import torch.nn as nn
+#imports=========================
