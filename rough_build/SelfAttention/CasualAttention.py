fda9d4034d0e26c5d5d524d739c26a49e99f20c6
Ivan Torriani <sotivantorriani@gmail.com>
2025-07-29 23:59:02 -0700

    feat *added init for casual attention

--- rough_build/SelfAttention/CasualAttention.py
+++ rough_build/SelfAttention/CasualAttention.py
@@ -9,3 +9,13 @@ purpose: Implement a casual attention class (self attention + masking )
 import torch
 import torch.nn as nn
 #imports=========================
+
+class CasualAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_keys = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_values = nn.Linear(d_in, d_out, bias=qkv_bias)
+        # also need to initialize mask and dropout
+        self.dropout = dropout 
+        self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=1))
