e88b09906e5c4381206d60519a4a6cad463bd788
Ivan Torriani <sotivantorriani@gmail.com>
2025-07-30 16:12:13 -0700

    feat *added multi attention class

---
+++ rough_build/Attention_Mechanisms/MultiHeadAttention.py
@@ -0,0 +1,16 @@
+#=========================
+'''
+file: MultiHeadAttention.py
+purpose: Implement multi head attention for more complicated tasks
+that might arise in the future
+note: i don't really know how this class works under the hood, 
+but it takes the same input as casual attention. 
+'''
+#=========================
+
+#imports=========================
+import torch
+import torch.nn as nn
+#imports=========================
+
+
