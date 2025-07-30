074b7ce07126b410586d2a84aa88c03429c258ad
Ivan Torriani <sotivantorriani@gmail.com>
2025-07-29 23:41:45 -0700

    feat *added self attetion init class

--- rough_build/SelfAttention/SelfAttentionv1.py
+++ rough_build/SelfAttention/SelfAttentionv1.py
@@ -13,3 +13,15 @@ import torch.nn as nn
 '''
 purpose: generalize attention mechanisms
 '''
+
+class SelfAttentionv1(nn.module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        '''
+        nn.Linear asserts the weight values, but also can be used to calcluate the queries 
+        values
+        '''
+    def forward(self, x):# where x is the tensor
