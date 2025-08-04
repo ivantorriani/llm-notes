458c82825fc947d27401f6129394bbe784973c4c
Ivan Torriani <sotivantorriani@gmail.com>
2025-08-03 22:48:01 -0700

    feat *Introduced transformer block

---
+++ rough_build/Architecture/TransfomerBlock.py
@@ -0,0 +1,48 @@
+
+#=========================
+'''
+file: TransformerBlock.py
+purpose: Introduces many previous architectures and mechanisms into one main block. 
+'''
+#=========================
+
+#imports=========================
+import torch
+import torch.nn as nn
+import rough_build.Architecture.FeedForward
+import rough_build.Attention_Mechanisms.MultiHeadAttention
+import rough_build.Architecture.LayerNorm 
+#imports=========================
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"], 
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"])
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self, x):
+        # Shortcut connection for attention block
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)  # Shape [batch_size, num_tokens, emb_size]
+        x = self.drop_shortcut(x)
+        x = x + shortcut  # Add the original input back
+
+        # Shortcut connection for feed forward block
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut  # Add the original input back
+
+        return x
