b72b41f213d29d1f727ae641fa4f830b3e820666
Ivan Torriani <sotivantorriani@gmail.com>
2025-08-08 14:29:07 -0700

    feat * renamed txt-data to data for structure *renamed rough_build to src for structure *added logs and checkpoints for structure

---
+++ src/Model_Analysis/cross_entropy.py
@@ -0,0 +1,47 @@
+#=========================
+'''
+file: cross_entropy.py
+purpose: Calculate the accuracy of the model's predictions to the targets. 
+note: functions included:
+calc_loss_batch
+calc_loss_loader
+'''
+#=========================
+
+#imports=========================
+import torch 
+import torch.nn as nn 
+
+#imports=========================
+
+'''
+purpose:
+calculate the loss of a specific batch. 
+'''
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch, target_batch = input_batch.to(device), target_batch.to(device)
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy(logits.flatten(0, 1), target_batch.flatten())
+    return loss 
+
+'''
+purpose:
+calculate the loss of entire dataset (more general). 
+'''
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        # Reduce the number of batches to match the total number of batches in the data loader
+        # if num_batches exceeds the number of batches in the data loader
+        num_batches = min(num_batches, len(data_loader))
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            total_loss += loss.item()
+        else:
+            break
+    return total_loss / num_batches
